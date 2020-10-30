71a37c302ca1e032b648d14cd176f1d715c71d0f
ramya <ramya.rolly@gmail.com>
2020-10-30 15:07:43 -0400

    covid-19 trackor app

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAqlhl3j_vN7N7Vi2OZTMP_w0f0-HteIlI",
+    authDomain: "covid-19-tracor.firebaseapp.com",
+    databaseURL: "https://covid-19-tracor.firebaseio.com",
+    projectId: "covid-19-tracor",
+    storageBucket: "covid-19-tracor.appspot.com",
+    messagingSenderId: "45845749105",
+    appId: "1:45845749105:web:076d77818a6cb9562eaad0",
+    measurementId: "G-JZYBYY9BX3"
+};
